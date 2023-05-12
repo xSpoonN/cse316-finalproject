e6f85d553882bd811a6231234db23bf19648cd69
Kevin Tao <kevin.tao@stonybrook.edu>
2023-05-12 19:15:02 -0400

    reorganize axiosmodel

--- client/src/models/axiosmodel.js
+++ client/src/models/axiosmodel.js
@@ -1,120 +1,34 @@
 import axios from 'axios'
 
-export async function addViews (qid) {
+/* ************* VIEWS ************* */
+export async function addViews (qid) { /* Increments the views of a question */
   await axios.post(`http://localhost:8000/questions/${qid}/views`)
 }
 
-export function removeView (qid) {
+export function removeView (qid) { /* Decrements the views of a question */
   axios.post(`http://localhost:8000/questions/${qid}/removeviews`)
 }
 
-export function getViews (qid) {
+export function getViews (qid) { /* Gets the views of a question */
   return axios.get(`http://localhost:8000/questions/${qid}`)
 }
 
-export async function getTagName (tagId) {
+//
+//
+//
+
+/* ************* TAGS ************* */
+export async function getTagName (tagId) { /* Gets the name of a tag */
   const resp = await axios.get(`http://localhost:8000/tags/${tagId}`)
   return resp.data.name
 }
 
-export async function getAllTags () {
+export async function getAllTags () { /* Gets all tags */
   const resp = await axios.get('http://localhost:8000/tags')
   return resp.data
 }
 
-export async function getAnswerCount (qid) {
-  const resp = await axios.get(`http://localhost:8000/questions/${qid}`)
-  console.log(resp.data.answers.length)
-  return resp.data.answers.length
-}
-
-export async function getAnswerFromId (aid) {
-  const resp = await axios.get(`http://localhost:8000/answers/${aid}`)
-  return resp.data
-}
-
-export async function getAnswersByQID (qid) {
-  const resp = await axios.get(`http://localhost:8000/questions/${qid}`)
-  console.log(resp)
-  const answers = await Promise.all(resp.data.answers.map(async (r) => {
-    const ans = await getAnswerFromId(r)
-    return ans
-  }))
-  return answers
-}
-
-export function getQuestions () {
-  return axios.get('http://localhost:8000/questions').then((response) => {
-    /* console.log(response.data) */
-    return response.data
-  }).catch((e) => {
-    console.error(e)
-  })
-}
-
-export function getQuestion (qid) {
-  return axios.get(`http://localhost:8000/questions/${qid}`).then((response) => {
-    console.log(response.data)
-    return response.data
-  }).catch((e) => {
-    console.error(e)
-  })
-}
-
-export function loginUser (email, password) {
-  return axios.post('http://localhost:8000/userLogin/', {
-    email,
-    password
-  }).then((response) => {
-    /* console.log(response.data) */
-    return response
-  }).catch((e) => {
-    console.error(e)
-    throw e
-  })
-}
-
-export function getAnswers () {
-  return axios.get('http://localhost:8000/answers').then((response) => {
-    /* console.log(response.data) */
-    return response.data
-  }).catch((e) => {
-    console.error(e)
-  })
-}
-
-export function getCommentsByAID (aid) {
-  return axios.get(`http://localhost:8000/comments/${aid}`).then((response) => {
-    /* console.log(response.data) */
-    return response
-  }).catch((e) => {
-    console.error(e)
-  })
-}
-
-export function getCommentByID (cid) {
-  return axios.get(`http://localhost:8000/comment/${cid}`).then((response) => {
-    /* console.log(response.data) */
-    return response
-  }).catch((e) => {
-    console.error(e)
-  })
-}
-
-export function addComment (aid, text, user) {
-  return axios.post('http://localhost:8000/comments', {
-    text,
-    aid,
-    user
-  }).then((response) => {
-    console.log(response.data)
-    return response.data
-  }).catch((e) => {
-    console.error(e)
-  })
-}
-
-export function getTags () {
+export function getTags () { /* Gets all tags */
   return axios.get('http://localhost:8000/tags').then((response) => {
     console.log(response.data)
     return response.data
@@ -123,7 +37,7 @@ export function getTags () {
   })
 }
 
-export function tagExists (tagName) {
+export function tagExists (tagName) { /* Checks if a tag exists */
   return axios.get(`http://localhost:8000/tagNames/${tagName}`).then((response) => {
     console.log(response.data)
     return response.data
@@ -132,20 +46,7 @@ export function tagExists (tagName) {
   })
 }
 
-export function addUser (email, username, password) {
-  return axios.post('http://localhost:8000/users', {
-    email,
-    username,
-    password
-  }).then((response) => {
-    console.log(response.data)
-    return response.data
-  }).catch((e) => {
-    console.error(e)
-  })
-}
-
-export function addTag (tag) {
+export function addTag (tag) { /* Adds a tag */
   return axios.post('http://localhost:8000/tags', {
     name: tag
   }).then((response) => {
@@ -156,22 +57,34 @@ export function addTag (tag) {
   })
 }
 
-export function addQuestion (title, text, tags, user) {
-  return axios.post('http://localhost:8000/questions', {
-    title,
-    text,
-    tags,
-    user
-  }).then((response) => {
-    console.log(response.data)
-    return response.data
-  }).catch((e) => {
-    console.error(e)
-  })
+//
+//
+//
+
+/* ************* ANSWERS ************* */
+export async function getAnswerCount (qid) { /* Gets the number of answers for a question */
+  const resp = await axios.get(`http://localhost:8000/questions/${qid}`)
+  console.log(resp.data.answers.length)
+  return resp.data.answers.length
+}
+
+export async function getAnswerFromId (aid) { /* Gets an answer from an id */
+  const resp = await axios.get(`http://localhost:8000/answers/${aid}`)
+  return resp.data
+}
+
+export async function getAnswersByQID (qid) { /* Gets all answers for a question */
+  const resp = await axios.get(`http://localhost:8000/questions/${qid}`)
+  /* console.log(resp) */
+  const answers = await Promise.all(resp.data.answers.map(async (r) => {
+    const ans = await getAnswerFromId(r)
+    return ans
+  }))
+  return answers
 }
 
 // eslint-disable-next-line camelcase
-export async function addAnswer (qid, ans_by, text) {
+export async function addAnswer (qid, ans_by, text) { /* Adds an answer */
   let newAnsId = null
 
   // Post a new answer
@@ -205,11 +118,111 @@ export async function addAnswer (qid, ans_by, text) {
   })
 }
 
-export async function getQuestionCountByTagId (tagId) {
+export function getAnswers () { /* Gets all answers */
+  return axios.get('http://localhost:8000/answers').then((response) => {
+    /* console.log(response.data) */
+    return response.data
+  }).catch((e) => {
+    console.error(e)
+  })
+}
+
+//
+//
+//
+
+/* ************* QUESTIONS ************* */
+export function getQuestions () { /* Gets all questions */
+  return axios.get('http://localhost:8000/questions').then((response) => {
+    /* console.log(response.data) */
+    return response.data
+  }).catch((e) => {
+    console.error(e)
+  })
+}
+
+export function getQuestion (qid) { /* Gets a question from an id */
+  return axios.get(`http://localhost:8000/questions/${qid}`).then((response) => {
+    console.log(response.data)
+    return response.data
+  }).catch((e) => {
+    console.error(e)
+  })
+}
+
+export function addQuestion (title, text, tags, user) { /* Adds a question */
+  return axios.post('http://localhost:8000/questions', {
+    title,
+    text,
+    tags,
+    user
+  }).then((response) => {
+    console.log(response.data)
+    return response.data
+  }).catch((e) => {
+    console.error(e)
+  })
+}
+
+export async function getQuestionCountByTagId (tagId) { /* Gets the number of questions for a tag */
   return (await getQuestions()).filter((q) => q.tags.includes(tagId)).length
 }
 
-export function getUser (email) {
+//
+//
+//
+
+/* ************* COMMENTS ************* */
+export function getCommentsByAID (aid) { /* Gets all comments for an answer */
+  return axios.get(`http://localhost:8000/comments/${aid}`).then((response) => {
+    /* console.log(response.data) */
+    return response
+  }).catch((e) => {
+    console.error(e)
+  })
+}
+
+export function getCommentByID (cid) { /* Gets a comment from an id */
+  return axios.get(`http://localhost:8000/comment/${cid}`).then((response) => {
+    /* console.log(response.data) */
+    return response
+  }).catch((e) => {
+    console.error(e)
+  })
+}
+
+export function addComment (aid, text, user) { /* Adds a comment to an answer */
+  return axios.post('http://localhost:8000/comments', {
+    text,
+    aid,
+    user
+  }).then((response) => {
+    /* console.log(response.data) */
+    return response.data
+  }).catch((e) => {
+    console.error(e)
+  })
+}
+
+//
+//
+//
+
+/* ************* USERS ************* */
+export function addUser (email, username, password) { /* Adds a user */
+  return axios.post('http://localhost:8000/users', {
+    email,
+    username,
+    password
+  }).then((response) => {
+    console.log(response.data)
+    return response.data
+  }).catch((e) => {
+    console.error(e)
+  })
+}
+
+export function getUser (email) { /* Gets a user from an email */
   return axios.get(`http://localhost:8000/users/${email}`).then((response) => {
     // console.log(response.data)
     return response.data
@@ -218,7 +231,20 @@ export function getUser (email) {
   })
 }
 
-export function formatDate (askDate, now = new Date()) {
+export function loginUser (email, password) { /* Logs in a user */
+  return axios.post('http://localhost:8000/userLogin/', {
+    email,
+    password
+  }).then((response) => {
+    /* console.log(response.data) */
+    return response
+  }).catch((e) => {
+    console.error(e)
+    throw e
+  })
+}
+
+export function formatDate (askDate, now = new Date()) { /* Formats a date */
   const timeDiffInSeconds = (now.getTime() - askDate.getTime()) / 1000
   const timeDiffInMinutes = timeDiffInSeconds / 60
   const timeDiffInHours = timeDiffInMinutes / 60
